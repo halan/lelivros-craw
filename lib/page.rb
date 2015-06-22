20bae666b5c56452f20aac89ca287312b79e0337
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2015-06-22 20:29:37 -0300

    Dont visit page book thats have already downloaded

--- lib/page.rb
+++ lib/page.rb
@@ -4,7 +4,9 @@ class PageCraw
   end
 
   def links
-    @page.search('[data-product_id]').map { |link| link[:href] }
+    @page.search('.product').reject do |li|
+      Dir["downloads/**/#{li.search('h3').text.gsub('/', '|')}.mobi"].any?
+    end.map{|li| li.search('[data-product_id]').first[:href] }
   end
 
   def next_page
