83a9bb7f9bdfd0e370ec5e149f3bf7371be9c408
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2015-06-22 23:20:44 -0300

    Add colors!

--- lib/reporter.rb
+++ lib/reporter.rb
@@ -1,17 +1,24 @@
 class Reporter
   def start_area(title)
-    puts "===> #{title}"
+    puts blue "===> #{title}"
   end
 
   def download_ok(title)
-    puts "-----> #{title}"
+    puts green "-----> #{title}"
   end
 
   def download_error(title)
-    warn "Erro - #{title}"
+    warn red "Erro - #{title}"
   end
 
   def download_exists(title)
-    puts "!----- #{title}"
+    puts brown "!----- #{title}"
   end
+
+  private
+
+  def red(s);   "\033[31m#{s}\033[0m" end
+  def green(s); "\033[32m#{s}\033[0m" end
+  def brown(s); "\033[33m#{s}\033[0m" end
+  def blue(s);  "\033[34m#{s}\033[0m" end
 end
