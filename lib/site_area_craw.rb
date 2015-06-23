21901b08dac5f23d5fe923a6fc4ac6a6ce64057f
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2015-06-23 20:50:29 -0300

    Improve main loop

--- lib/site_area_craw.rb
+++ lib/site_area_craw.rb
@@ -9,15 +9,16 @@ class SiteAreaCraw
 
   def craw!
     @reporter.start_area(@title)
-
-    all_urls.each do |url|
-      books = BooksCraw.new(@format, @reporter)
-      books.append PageCraw.new(@browser.get(url), @format).links
-      books.download!("downloads/#{@title}")
-    end
+    all_urls.each { |url| navigate_and_download(url) }
   end
 
   protected
+
+  def navigate_and_download(url)
+    books = BooksCraw.new(@format, @reporter)
+    books.append PageCraw.new(@browser.get(url), @format).links
+    books.download!("downloads/#{@title}")
+  end
 
   def pages_count
     @pages_count ||= @browser.get(@url).search('.pages').text[/[0-9]+$/].to_i
