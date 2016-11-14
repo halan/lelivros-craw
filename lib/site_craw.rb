22dacdac044d4f146fd92e2542b391eff0856c24
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2016-11-14 15:15:53 -0200

    Small fixes about new domain

--- lib/site_craw.rb
+++ lib/site_craw.rb
@@ -8,11 +8,11 @@ class SiteCraw
 
   def craw!
     craw_areas!
-    @areas.each { |area| SiteAreaCraw.new(area[:href], area[:title], @format, @reporter).craw! }
+    @areas.each { |area| SiteAreaCraw.new("#{@url}#{area[:href]}", area[:title], @format, @reporter).craw! }
   end
 
   def craw_areas!
-    @areas = @browser.get('http://lelivros.red/').
+    @areas = @browser.get(@url).
       search('.cat-item a').map{|a| {title: a.text, href: a[:href] } }
   end
 end
